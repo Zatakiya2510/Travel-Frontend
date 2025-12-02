0a824a85d39d766355a84f01e90bb07a71b01968
Abhishek Zatakiya <114485563+Zatakiya2510@users.noreply.github.com>
2025-12-02 22:28:52 +0530

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,3 +1,3 @@
 // export const BASE_URL1 = 'https://travel-backend-1-fgwb.onrender.com/api/v1';
 //export const BASE_URL1 = 'https://tour-backend-i1a8.onrender.com/api/v1';
-export const BASE_URL1 = 'http://localhost:4000/api/v1';
+export const BASE_URL1 = 'https://tour-backend-i1a8.onrender.com/api/v1';
